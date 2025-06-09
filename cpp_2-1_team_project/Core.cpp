85a1e05c9dc5cafda537675357b03a732d9d8e65
dntlakfn <83486067+dntlakfn@users.noreply.github.com>
2025-06-09 14:40:26 +0900

    feat

--- cpp_2-1_team_project/Core.cpp
+++ cpp_2-1_team_project/Core.cpp
@@ -1,8 +1,11 @@
 #include "Core.h"
 #include "Enums.h"
+#include "Console.h"
 
 void Core::Run()
 {
+	Console console = Console();
+	console.SetConsoleSettings(800, 600, true, TEXT("2-1 Team Project"));
 	while (true)
 	{
 		Update();
