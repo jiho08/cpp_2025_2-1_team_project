378ce675e50b773ec23ab765682cdd92888acf75
jiho08 <57872222k@gmail.com>
2025-06-18 23:26:51 +0900

    feat : player wall limit

--- cpp_2-1_team_project/GameScene.h
+++ cpp_2-1_team_project/GameScene.h
@@ -1,28 +1,20 @@
 #pragma once
-#include <string>
 #include <vector>
-#include <fstream>
 #include <iostream>
-#include <filesystem>
-#include "Console.h"
-#include "defines.h"
 #include "Enums.h"
 #include "Scene.h"
 #include "Player.h"
-#include "KeyController.h"
-#include "SceneManager.h"
-#include "TitleScene.h"
+
 using std::cout;
 using std::endl;
 using std::vector;
-
 
 class GameScene : public Scene
 {
 public:
 	GameScene();
+	~GameScene() override;
 
-	~GameScene();
 	int stage = 1;
 	void Init() override;
 	void Update() override;
--- cpp_2-1_team_project/Player.h
+++ cpp_2-1_team_project/Player.h
@@ -1,24 +1,31 @@
 #pragma once
 #include <string>
+#include <vector>
 #include "Object.h"
 #include "Structs.h"
 #include "IMovable.h"
 
 using std::string;
+using std::vector;
 
-class Player : public Object, public IMovable
+class Player final : public Object, public IMovable
 {
 	string _playerSymbol = "¢Ã";
 	COLOR _currentColor;
+	const vector<vector<char>>* _map;
 
 public:
-	Player(Position startPos = { 0, 0 });
+	Player(Position pos = { 0, 0 });
 
-	string GetSymbol() const;
-	COLOR GetColor() const;
-	void SetColor(COLOR newColor);
 	void Update() override;
 	void Render() const override;
 	void Move(DIR dir) override;
+
 	void SetPosition(Position newPos);
+
+	string GetSymbol() const;
+	COLOR GetColor() const;
+
+	void SetColor(COLOR newColor);
+	void SetMap(const vector<vector<char>>* map);
 };
